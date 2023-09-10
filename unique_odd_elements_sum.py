5dde3549825a9dcece0b3bd61a85894f7cc7715b
sivashankarbodapati <119917360+sivashankarbodapati@users.noreply.github.com>
2023-09-10 09:24:02 +0530

    def unique(arr):   ulst = []   for x in arr:     if x not in ulst:        if x%2 !=0:           ulst.append(x)   return ulst t = int(input()) arr = list(map(int,input().split())) res = unique(arr) print(sum(res))

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,11 @@
+def unique(arr):
+  ulst = []
+  for x in arr:
+    if x not in ulst:
+       if x%2 !=0:
+          ulst.append(x)
+  return ulst
+t = int(input())
+arr = list(map(int,input().split()))
+res = unique(arr)
+print(sum(res))
